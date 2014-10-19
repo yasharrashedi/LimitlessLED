147d2a5b602a538b2410b1de3452a168a287159d
Yashar Rashedi <info@rashedi.com>
2014-10-19 11:33:07 +0330

    changed example (added some method calls)

--- example.php
+++ example.php
@@ -8,15 +8,18 @@ $milight = new Milight('192.168.0.54');
 
 $milight->rgbwAllOn();
 $milight->rgbwAllSetToWhite();
+$milight->rgbwAllBrightnessMax();
 sleep(2);
 $milight->setRgbwActiveGroup(1);
 $milight->rgbwBrightnessPercent(50);
 sleep(2);
 $milight->setRgbwActiveGroup(2);
-$milight->rgbwSetColorHexString('FF1254');
+$milight->rgbwSetColorHexString('FF1254'); // or #FF1254
 $milight->rgbwBrightnessPercent(90);
 sleep(2);
 $milight->whiteAllOn();
+$milight->whiteAllBrightnessMax();
+sleep(2);
 $milight->whiteGroup1NightMode();
 sleep(2);
 $milight->setWhiteActiveGroup(1);
